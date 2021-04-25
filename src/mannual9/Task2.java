9aecb67ce80a17a9976edd68afffbb5efa0ab3a9
Muneeb912-cmd <emanmuneebsam@gmail.com>
2021-04-25 16:22:38 +0500

    Task2

--- src/mannual9/Task2.java
+++ src/mannual9/Task2.java
@@ -10,5 +10,16 @@ package mannual9;
  * @author muzammal computer
  */
 public class Task2 {
-    
+    /**
+     * displays the input string ion versicle
+     * no return type as function is void
+     * @param s 
+     */
+        void VerticleStringDisplayer(String s){
+        char ch_a[]=s.toCharArray();
+        int size=ch_a.length;
+        for(int i=0;i<size;i++){
+            System.out.print(ch_a[i]+"\n");
+        }
+        }
 }
