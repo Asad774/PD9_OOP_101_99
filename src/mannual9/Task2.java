ddb7ae273c27a371ef05433d6e76f26019a8a3c4
Asad774 <81927489+Asad774@users.noreply.github.com>
2021-04-25 17:32:21 -0700

    Asad Bhatti 4

--- src/mannual9/Task2.java
+++ src/mannual9/Task2.java
@@ -21,9 +21,7 @@ public class Task2 {
 //        for(int i=0;i<size;i++){
 //            System.out.print(ch_a[i]+"\n");
 //        }
-//        }
-//        
-//        
+//        }        
         
         public void displayVertical(String s)
   {
--- src/mannual9/Task3.java
+++ src/mannual9/Task3.java
@@ -10,5 +10,50 @@ package mannual9;
  * @author muzammal computer
  */
 public class Task3 {
+ 
     
+      
+  public String ridMultipleBlank(String s)
+  {
+    String [] tokens = s.split(" ");
+    String str=null;
+    for(int i=0; i<tokens.length ;i++)
+    {
+      if(i==0)
+      {
+      if(!tokens[i].equals(""))
+      {
+        str=tokens[i]+" ";
+      }
+      }
+      else
+      {
+       if(!tokens[i].equals(""))
+      {
+        str=str+tokens[i]+" ";
+      }
+      } 
+    }
+    return str;
+  } 
+  
+  public String removeInteger(String s)
+  {
+    int b=s.length();
+    String str="";
+    for(int i=0; i<b;i++)
+    {
+      if(s.charAt(i)>='0' && s.charAt(i)<='9')
+      {
+
+      } 
+      else
+      {
+       str=str+s.charAt(i); 
+      }
+    }
+    return str;
+  } 
+  
+  
 }
